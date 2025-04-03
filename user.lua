02b44bcb1cfb707fdae6483e9354e54ca2df4744
Ruijie Gao <wuliJerry927@gmail.com>
2025-04-03 19:03:52 +0800

    update

--- user.lua
+++ user.lua
@@ -8,7 +8,7 @@ function M.setup_neovide()
 
     vim.g.neovide_floating_blur_amount_x = 2.0
     vim.g.neovide_floating_blur_amount_y = 2.0
-    vim.g.neovide_transparency = 0.9
+    vim.g.neovide_opacity = 0.9
     vim.g.transparency = 0.8
 
     vim.g.neovide_cursor_animation_length = 0
